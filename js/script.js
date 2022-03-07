82f7cda1a5a5f66dba7a58391ef1bf8f49d12eb1
Diana Ramirez <37516634+dianaramirez16@users.noreply.github.com>
2022-03-07 09:50:29 -0800

    sidebar now collapses

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 
 btn.onclick= function(){
-    btn.classList.toggle("active");
+    sidebar.classList.toggle("active");
 }
